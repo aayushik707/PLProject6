f320297ad67f95c80e33a6b790fd1e7a01d3c58f
Aayushi <171912740+aayushik707@users.noreply.github.com>
2024-12-09 22:44:53 -0500

    Update astbkp.hs

--- ast/astbkp.hs
+++ ast/astbkp.hs
@@ -1,35 +1,35 @@
 module Ast where
 
-
+-- Define the data type for arithmetic expressions
 data Expr
-    = Literal Float           -- Represents a literal value
+    = Literal Float           -- Represents a numeric literal
     | Plus Expr Expr          -- Addition
     | Minus Expr Expr         -- Subtraction
     | Times Expr Expr         -- Multiplication
     | Div Expr Expr           -- Division
-    deriving (Show)
+    deriving (Show)           -- Enable printing of expressions for debugging
 
--- Evaluate the expression
+-- Evaluate an expression
 eval :: Expr -> Float
-eval (Literal x)     = x
-eval (Plus e1 e2)    = eval e1 + eval e2
-eval (Minus e1 e2)   = eval e1 - eval e2
-eval (Times e1 e2)   = eval e1 * eval e2
-eval (Div e1 e2)     = eval e1 / eval e2
+eval (Literal x)       = x
+eval (Plus left right) = eval left + eval right
+eval (Minus left right) = eval left - eval right
+eval (Times left right) = eval left * eval right
+eval (Div left right)   = eval left / eval right
 
-
+-- Test expressions and their evaluations
 test1 :: Float
-test1 = eval $ Plus (Literal 3.0) (Literal 2.0)
+test1 = eval $ Plus (Literal 3.0) (Literal 2.0)  -- Expected: 5.0
 
 test2 :: Float
-test2 = eval $ Plus (Literal 3.0) (Div (Literal 1.0) (Literal 2.0))
+test2 = eval $ Plus (Literal 3.0) (Div (Literal 1.0) (Literal 2.0))  -- Expected: 3.5
 
 test3 :: Float
-test3 = eval $ Plus (Times (Literal 3.0) (Literal 5.0)) (Div (Literal 1.0) (Literal 2.0))
+test3 = eval $ Plus (Times (Literal 3.0) (Literal 5.0)) (Div (Literal 1.0) (Literal 2.0))  -- Expected: 15.5
 
--- Main function to output the results of the tests
+-- Main function to output results
 main :: IO ()
 main = do
-    print test1  -- Expected output: 5.0
-    print test2  -- Expected output: 3.5
-    print test3  -- Expected output: 15.5
+    print test1  -- Output: 5.0
+    print test2  -- Output: 3.5
+    print test3  -- Output: 15.5
